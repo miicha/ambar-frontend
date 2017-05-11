6732237f7107941ce6e890132064c73b35f84ef9
Igor.S <fpd4444@gmail.com>
2017-05-11 16:41:03 +0300

    Chinese lang analyzer added

--- src/routes/AccountPage/modules/AccountPage.js
+++ src/routes/AccountPage/modules/AccountPage.js
@@ -264,7 +264,11 @@ export const loadUserInfo = () => {
                                     ? 'Polish'
                                     : data.lang_analyzer === 'ambar_cjk'
                                         ? 'CJK'
-                                        : 'Berberian'
+                                        : data.lang_analyzer === 'ik_max_word'
+                                            ? 'Chinese (ik_max_word)'
+                                            : data.lang_analyzer === 'ik_smart'
+                                                ? 'Chinese (ik_smart)'
+                                                : 'Berberian'
 
                 let plan = data.plan === 'unlim'
                     ? 'Unlimited'
