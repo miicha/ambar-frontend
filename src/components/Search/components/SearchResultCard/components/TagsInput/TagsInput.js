32ed05b8e464d0d91ace58616f703a2d56c72275
Ilya P <ilya.pir@gmail.com>
2017-05-29 12:11:27 +0300

    Tags styling

--- src/components/Search/components/SearchResultCard/components/TagsInput/TagsInput.js
+++ src/components/Search/components/SearchResultCard/components/TagsInput/TagsInput.js
@@ -3,27 +3,38 @@ import React, { Component } from 'react'
 import AutosizeInput from 'react-input-autosize'
 import Autosuggest from 'react-autosuggest'
 
+import ClearIcon from 'material-ui/svg-icons/content/clear'
+import FontIcon from 'material-ui/FontIcon'
+
 import classes from './TagsInput.scss'
 
-const Tag = ({children, onRemove}) => {
-    const onRemoveCallback = onRemove 
+const Tag = ({ children, onRemove }) => {
+    const onRemoveCallback = onRemove
         ? onRemove
-        : () => {}
+        : () => { }
 
     return (
-        <span 
-            style={{padding: '4px', cursor: 'pointer', display: 'inline-block', 'word-wrap': 'break-word', 'word-break': 'break-all'}}
-            onClick={() => console.log('clicked')}>
-            {children}
-            <span 
-                onClick={() => onRemoveCallback(children)}
-                style={{color: 'red', cursor: 'pointer', padding: '3px'}}
-            >x</span>
-        </span>
+        <div
+            style={{display: 'flex', alignItems: 'center'}}            
+            className={classes.tag}
+            onTouchTap={(e) => {
+                console.log('clicked', e)}
+            }>
+            <span>{children}</span>
+            <ClearIcon 
+                className={classes.removeTagButton} 
+                onTouchTap={(e) => {
+                    e.stopPropagation()
+                    onRemoveCallback(children)
+                }}                                   
+                hoverColor='#FF5722'
+                style={{color: 'inherit', width: '1em', height: '1em'}}        
+            />            
+        </div>
     )
 }
 
-Tag.propTypes = {    
+Tag.propTypes = {
     children: React.PropTypes.string.isRequired,
     onRemove: React.PropTypes.func
 }
@@ -48,10 +59,10 @@ class TagsInput extends Component {
 
         if (this.state.tags.indexOf(tag) == -1) {
             const tags = [...this.state.tags, tag]
-            this.setState({...this.state, tags: tags, inputValue: ''})  
+            this.setState({ ...this.state, tags: tags, inputValue: '' })
         } else {
-            this.setState({...this.state, inputValue: ''})     
-        }  
+            this.setState({ ...this.state, inputValue: '' })
+        }
     }
 
     removeTag(tag) {
@@ -60,8 +71,8 @@ class TagsInput extends Component {
         if (!currentTags || currentTags.length === 0) {
             return
         }
-        
-        this.setState({...this.state, tags: currentTags.filter(t => t !== tag)})
+
+        this.setState({ ...this.state, tags: currentTags.filter(t => t !== tag) })
     }
 
     removeLastTag() {
@@ -69,29 +80,29 @@ class TagsInput extends Component {
         if (!currentTags || currentTags.length === 0) {
             return
         }
-        
-        this.setState({...this.state, tags: currentTags.slice(0, currentTags.length - 1)})
+
+        this.setState({ ...this.state, tags: currentTags.slice(0, currentTags.length - 1) })
     }
 
-    onChange(value) {        
-        this.setState({...this.state, inputValue: value })
+    onChange(value) {
+        this.setState({ ...this.state, inputValue: value })
 
         const newTags = value.split(',').map(t => t.trim().toLowerCase())
-        
-        if (newTags.length > 1 && newTags[0] != '')  {
-            this.addTag(newTags[0])            
-        }        
+
+        if (newTags.length > 1 && newTags[0] != '') {
+            this.addTag(newTags[0])
+        }
     }
 
-    onKeyPress(event) {        
+    onKeyPress(event) {
         if (event.charCode === 13) { // onEnter
             this.addTag(this.state.inputValue)
             return
-        }        
+        }
     }
 
-    onKeyDown(event) {        
-       if (event.keyCode === 8) { // onBackspace            
+    onKeyDown(event) {
+        if (event.keyCode === 8) { // onBackspace            
             if (!this.state.inputValue) {
                 this.removeLastTag()
                 return
@@ -103,32 +114,33 @@ class TagsInput extends Component {
         this.addTag(this.state.inputValue)
     }
 
-    focusOnInput() {
-        console.log(this.refs.suggestInput)
+    focusOnInput() {        
         if (!this.refs.suggestInput) {
             return
         }
 
         this.refs.suggestInput.input.focus()
     }
-    
+
     autosizeInput(inputProps) {
         return (
-            <AutosizeInput                            
-                minWidth={40}                               
+            <AutosizeInput
+                minWidth={40}
                 inputStyle={{
-                    'border':'none',
-                    'background-image':'none',
-                    'background-color':'transparent',
+                    'border': 'none',
+                    'background-image': 'none',
+                    'background-color': 'transparent',
                     '-webkit-box-shadow': 'none',
                     '-moz-box-shadow': 'none',
                     'box-shadow': 'none',
-                    'outline': 'none'
+                    'outline': 'none',
+                    margin: '4px',
+                    'padding': '0'
                 }}
-                type='text'     
-                placeholder='Start typing to add tags'             
-                {...inputProps}        
-            /> 
+                type='text'
+                placeholder='type here to add tags'
+                {...inputProps}
+            />
         )
     }
 
@@ -157,99 +169,100 @@ class TagsInput extends Component {
         const props = this.props
 
         const theme = {
-             
-  container: {
-      display: 'inline-block',
-    position: 'relative'
-  },
-  /*
-  input: {
-    width: 240,
-    height: 30,
-    padding: '10px 20px',
-    fontFamily: 'Helvetica, sans-serif',
-    fontWeight: 300,
-    fontSize: 16,
-    border: '1px solid #aaa',
-    borderTopLeftRadius: 4,
-    borderTopRightRadius: 4,
-    borderBottomLeftRadius: 4,
-    borderBottomRightRadius: 4,
-  },
-  inputFocused: {
-    outline: 'none'
-  },
-  */
-  inputOpen: {
-    borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0
-  },
-  suggestionsContainer: {
-    display: 'none'
-  },
-  suggestionsContainerOpen: {
-    display: 'block',
-    position: 'absolute',
-    top: 30,
-    width: 280,
-    border: '1px solid #aaa',
-    backgroundColor: '#fff',
-    fontFamily: 'Helvetica, sans-serif',
-    fontWeight: 300,
-    fontSize: 16,
-    borderBottomLeftRadius: 4,
-    borderBottomRightRadius: 4,
-    zIndex: 2
-  },
-  suggestionsList: {
-    margin: 0,
-    padding: 0,
-    listStyleType: 'none',
-  },
-  suggestion: {
-    cursor: 'pointer',
-    padding: '10px 20px'
-  },
-  suggestionHighlighted: {
-    backgroundColor: '#ddd'
-  }
-}
+
+            container: {
+                display: 'inline-block',
+                position: 'relative',
+                margin: '4px'
+            },
+            /*
+            input: {
+              width: 240,
+              height: 30,
+              padding: '10px 20px',
+              fontFamily: 'Helvetica, sans-serif',
+              fontWeight: 300,
+              fontSize: 16,
+              border: '1px solid #aaa',
+              borderTopLeftRadius: 4,
+              borderTopRightRadius: 4,
+              borderBottomLeftRadius: 4,
+              borderBottomRightRadius: 4,
+            },
+            inputFocused: {
+              outline: 'none'
+            },
+            */
+            inputOpen: {
+                borderBottomLeftRadius: 0,
+                borderBottomRightRadius: 0
+            },
+            suggestionsContainer: {
+                display: 'none'
+            },
+            suggestionsContainerOpen: {
+                display: 'block',
+                position: 'absolute',
+                top: 30,
+                width: 280,
+                border: '1px solid #aaa',
+                backgroundColor: '#fff',
+                fontFamily: 'Helvetica, sans-serif',
+                fontWeight: 300,
+                fontSize: 16,
+                borderBottomLeftRadius: 4,
+                borderBottomRightRadius: 4,
+                zIndex: 2
+            },
+            suggestionsList: {
+                margin: 0,
+                padding: 0,
+                listStyleType: 'none',
+            },
+            suggestion: {
+                cursor: 'pointer',
+                padding: '10px 20px'
+            },
+            suggestionHighlighted: {
+                backgroundColor: '#ddd'
+            }
+        }
 
 
-        return (     
-            <div 
-                style={{padding: '5px', cursor: 'text'}}
-                onClick={() => this.focusOnInput()}>   
-                {this.state.tags.map((tag, idx) => 
+        return (
+            <div
+                style={{ padding: '5px', cursor: 'text', display: 'flex', flexWrap: 'wrap' }}
+                onClick={() => this.focusOnInput()}>
+                {this.state.tags.map((tag, idx) =>
                     <Tag key={idx} onRemove={(tag) => this.removeTag(tag)}>{tag}</Tag>)
-                }       
-                <Autosuggest 
+                }
+                <Autosuggest
                     ref='suggestInput'
-                    suggestions={this.state.suggestions}                    
+                    suggestions={this.state.suggestions}
                     getSuggestionValue={s => s}
                     renderSuggestion={(suggestion) => <span>{suggestion}</span>}
-                    onSuggestionSelected={(e, {suggestion}) => {
+                    onSuggestionSelected={(e, { suggestion }) => {
                         this.addTag(suggestion)
-                    }}                           
+                    }}
                     onSuggestionsFetchRequested={e => this.onSuggestionsFetchRequested(e)}
-                    onSuggestionsClearRequested={e => this.onSuggestionsClearRequested(e)}            
-                    inputProps = {{
+                    onSuggestionsClearRequested={e => this.onSuggestionsClearRequested(e)}
+                    inputProps={{
                         value: this.state.inputValue,
-                        onChange: (e) => this.onChange(e.target.value),                       
+                        onChange: (e) => this.onChange(e.target.value),
                         onBlur: () => this.onBlur(),
                         onKeyPress: (event) => this.onKeyPress(event),
                         onKeyDown: (event) => this.onKeyDown(event),
                         /*ref: (ref) => {console.log('here'); this.inputRef = ref}*/
                     }}
                     theme={theme}
-                    renderInputComponent={(inputProps) => this.autosizeInput(inputProps)}                    
-                />                                                       
+                    renderInputComponent={(inputProps) => this.autosizeInput(inputProps)}
+                />
             </div>
         )
     }
 }
 
-TagsInput.propTypes = {    
+TagsInput.propTypes = {
 }
 
 export default TagsInput
