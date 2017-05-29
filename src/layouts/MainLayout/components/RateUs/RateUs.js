18f2331c733a0221f6fba9bb4f8bf2ff7b8959ff
Ilya P <ilya.pir@gmail.com>
2017-05-29 16:16:55 +0300

    Minor changes to RateUs dialog

--- src/layouts/MainLayout/components/RateUs/RateUs.js
+++ src/layouts/MainLayout/components/RateUs/RateUs.js
@@ -20,48 +20,40 @@ class RateUs extends Component {
             window.open(url)
         }
 
-        const actions = [
-            <FlatButton
-                label="Follow"
-                primary={true}
-                onTouchTap={() => goToUrl('https://www.facebook.com/Ambar-Smart-Document-Archive-1494459153928382/')}
-                icon={<img height={20} src={FacebookIcon} />}
-            />,
-            <FlatButton
-                label="Star"
-                primary={true}
-                onTouchTap={() => goToUrl('https://github.com/RD17/ambar')}
-                icon={<img height={20} src={GithubIcon} />}
-            />,
-            <FlatButton
-                label="Follow"
-                primary={true}
-                onTouchTap={() => goToUrl('https://twitter.com/RD17Ambar')}
-                icon={<img height={20} src={TwitterIcon} />}
-            />
-        ]
-
         return (
             <div className={classes.stampIconContainer}>
                 <img className={classes.stampIcon} src='./stamp-icon.png' width='48' height='48' title='Rate Us' onClick={() => toggle(true)} />
                 <Dialog
                     title="Thanks for choosing Ambar!"
-                    actions={actions}                    
                     open={isOpen}
                     actionsContainerStyle={{ textAlign: 'center' }}
                     onRequestClose={() => toggle(false)}
                 >
-                    <div style={{display: 'flex', flexDirection: 'row'}}>
+                    <div style={{ display: 'flex', flexDirection: 'row' }}>
                         <div style={{ display: 'flex', justifyContent: 'center', marginRight: '20px' }}>
                             <img style={{ maxHeight: '250px' }} src='/rate-us-owl.gif' alt='Rate Us' />
                         </div>
                         <div className={classes.rateUsText}>
                             <p>
-                                Let's spread the word that Ambar is awesome! Help us make Ambar even better, follow us on Twitter and Facebook or give us you stars on Github. 
+                                Let's spread the word that Ambar is awesome! Help us make Ambar even better, follow us on Twitter or give us your stars on Github.
                             </p>
                             <p>
-                                Together we will build the best document management system in the world!
+                                Together we will build the best document search system in the world!
                             </p>
+                            <div style={{display: 'flex', justifyContent: 'center'}}>
+                                <FlatButton
+                                    label="Star"
+                                    primary={true}
+                                    onTouchTap={() => goToUrl('https://github.com/RD17/ambar')}
+                                    icon={<img height={20} src={GithubIcon} />}
+                                />
+                                <FlatButton
+                                    label="Tweet"
+                                    primary={true}
+                                    onTouchTap={() => goToUrl('https://twitter.com/intent/tweet?text=%23Ambar%20is%20awesome%20%23DocumentSearchSystem!%20Check%20it%20out%20on%20https%3A%2F%2Fambar.cloud')}
+                                    icon={<img height={20} src={TwitterIcon} />}
+                                />
+                            </div>
                         </div>
                     </div>
                 </Dialog>
